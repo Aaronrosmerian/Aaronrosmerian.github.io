644e726cb91df10bedbedf424953e6190a071f5b
Sir A <alfinfirdaus51@gmail.com>
2025-01-22 10:52:34 +0700

    Add files via upload

---
+++ ppt/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const pptSection = document.getElementById('ppt-content');
+    pptSection.innerHTML += "<p>Pastikan file PPT Anda telah diunggah ke folder <b>/ppt-content</b>.</p>";
+});
